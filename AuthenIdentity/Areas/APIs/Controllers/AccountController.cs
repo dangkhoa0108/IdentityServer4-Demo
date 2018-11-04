4f63c93fe6b503d083f68d9aa854257df0c7c84b
Trần Đăng Khoa <tdangkhoa.itute@gmail.com>
2018-11-04 15:15:20 +0700

    Set baerer token

--- AuthenIdentity/Areas/APIs/Controllers/AccountController.cs
+++ AuthenIdentity/Areas/APIs/Controllers/AccountController.cs
@@ -1,23 +1,31 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
 using AuthenIdentity.Models;
+using IdentityModel.Client;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 
 namespace AuthenIdentity.Areas.APIs.Controllers
 {
     [Area("APIs")]
+    [Route("apis/[controller]/[action]")]
     public class AccountController : Controller
     {
+        private readonly SignInManager<ApplicationUser> _signInManager;
         private readonly UserManager<ApplicationUser> _userManager;
         private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly IConfiguration _configuration;
 
         public AccountController(
             UserManager<ApplicationUser> userManager,
-            RoleManager<IdentityRole> roleManager
-        )
+            RoleManager<IdentityRole> roleManager, SignInManager<ApplicationUser> signInManager, IConfiguration configuration)
         {
             _userManager = userManager;
             _roleManager = roleManager;
+            _signInManager = signInManager;
+            _configuration = configuration;
         }
 
         [HttpPost]
@@ -54,5 +62,52 @@ namespace AuthenIdentity.Areas.APIs.Controllers
 
 
         }
+
+        [HttpPost]
+        public async Task<IActionResult> Test(string username, string password)
+        {
+            var disco = await DiscoveryClient.GetAsync("https://localhost:44321/");
+            if(disco.IsError) throw new Exception(disco.Error);
+            var client= new TokenClient(disco.TokenEndpoint, "ro.angular","secret");
+            var token= await client.RequestResourceOwnerPasswordAsync(username, password, "api1");
+            var a = new HttpClient();
+            a.SetBearerToken(token.AccessToken);
+            return new ObjectResult(a.GetStringAsync("https://localhost:44321/api/values/Get").Result);
+        }
+
+        //[HttpPost]
+        //public async Task<object> Login(string userName, string password)
+        //{
+        //    var result = await _signInManager.PasswordSignInAsync(userName, password, false, false);
+        //    if (result.Succeeded)
+        //    {
+        //        var appUser = _userManager.Users.SingleOrDefault(x => x.UserName == userName);
+        //        if (appUser != null) return await GenerateJwtToken(appUser.Email, appUser);
+        //    }
+        //    throw new ApplicationException("INVALID_LOGIN_ATTEMPT");
+        //}
+        //private async Task<object> GenerateJwtToken(string email, IdentityUser user)
+        //{
+        //    var claims = new List<Claim>
+        //    {
+        //        new Claim(JwtRegisteredClaimNames.Sub, email),
+        //        new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+        //        new Claim(ClaimTypes.NameIdentifier, user.Id)
+        //    };
+
+        //    var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["JwtKey"]));
+        //    var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+        //    var expires = DateTime.Now.AddDays(Convert.ToDouble(_configuration["JwtExpireDays"]));
+
+        //    var token = new JwtSecurityToken(
+        //        _configuration["JwtIssuer"],
+        //        _configuration["JwtIssuer"],
+        //        claims,
+        //        expires: expires,
+        //        signingCredentials: creds
+        //    );
+
+        //    return new JwtSecurityTokenHandler().WriteToken(token);
+        //}
     }
 }
